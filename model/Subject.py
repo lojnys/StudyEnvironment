074bdde33c4860622163d321a56895fc7734a9cd
lojnys <lojnys@students.cs.ubc.ca>
2023-01-06 20:40:37 -0800

    Added a Subject class

---
+++ model/Subject.py
@@ -0,0 +1,27 @@
+from Task import Task
+
+class Subject(Task):
+
+    """Represents a study task for a given subject"""
+    """This class can be used for keeping track of time left to study"""
+
+    # EFFECTS: constructs a study task with given subject name and a goal time of 2 hours
+    #          the goal time can be modified
+    def __init__(self, subName: str, goal: int = 2) -> None:
+        super().__init__(subName + " Study", 1)
+        self.goal = goal
+
+    # EFFECTS: returns the goal time of the study task
+    def getGoal(self) -> int:
+        return self.goal
+
+    # EFFECTS: sets the goal time as the given value
+    # MODIFIES: self
+    def setGoal(self, goal: int) -> None:
+        self.goal = goal
+
+    # overriding toJson() method from Task
+    def toJson(self) -> dict:
+        diction = super().toJson()
+        diction.update({"goal-time": self.getGoal()})
+        return diction
